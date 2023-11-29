68c2193f8ac19144193ba3ec085e16c62a421a02
marifsulaksono <marifsulaksono@gmail.com>
2023-11-30 00:10:52 +0700

    feat:add set to store data (cache)

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
 const redisHost = "localhost:6379"
 const redisPwd = ""
@@ -8,5 +12,18 @@ const redisPwd = ""
 func main() {
 	// initialize redis connection
 	dbRedis := Connect(redisHost, redisPwd)
-	fmt.Println("redis client initialized on", dbRedis.String())
+	fmt.Println("redis client initialized")
+
+	// storing data using set command
+	key := "greeting"
+	data := "Hi, I'm Muhammad Arif Sulaksono"
+	ttl := time.Duration(3) * time.Second
+
+	opSet := dbRedis.Set(context.Background(), key, data, ttl)
+	if err := opSet.Err(); err != nil {
+		fmt.Printf("failed to set data : %v", err)
+		return
+	}
+
+	fmt.Println("set operation success")
 }
